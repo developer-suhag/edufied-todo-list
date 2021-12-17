ceab4fb33bdfbe7ca21206fd117b630f4f25bfb1
Suhag Al Amin <developersuhag@gmail.com>
2021-12-17 17:27:23 +0600

    moved to todo list

--- src/components/Todos/SingleTodo.js
+++ src/components/Todos/SingleTodo.js
@@ -1,24 +1,28 @@
 import React from "react";
 import { Form } from "react-bootstrap";
 import { AiOutlineCheckCircle, AiOutlineDelete } from "react-icons/ai";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { markAsCompleted } from "../../redux/slices/todoSlice";
 
 const SingleTodo = ({ todo }) => {
   const dispatch = useDispatch();
+  const todoList = useSelector((state) => state.todos.todoList);
+  const { id, name } = todo;
   return (
     <div
       style={{ backgroundColor: "#edf6f9" }}
       className="mb-3 px-2 py-1 d-flex align-items-center rounded-3 shadow-sm"
     >
-      <p className="m-0 fs-5">{todo.name}</p>
+      <p className="m-0 fs-5">{name}</p>
       <div className="ms-auto">
-        <AiOutlineCheckCircle
-          onClick={() => dispatch(markAsCompleted(todo))}
-          style={{ color: "#006d77", cursor: "pointer" }}
-          title="Mark as completed"
-          className="fs-4 me-2"
-        />
+        {todoList.find((todo) => todo.id === id) && (
+          <AiOutlineCheckCircle
+            onClick={() => dispatch(markAsCompleted(todo))}
+            style={{ color: "#006d77", cursor: "pointer" }}
+            title="Mark as completed"
+            className="fs-4 me-2"
+          />
+        )}
         <AiOutlineDelete
           style={{ color: "#d90429", cursor: "pointer" }}
           title="Delete from todos"
